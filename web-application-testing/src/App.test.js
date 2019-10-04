a5cc8f267a83491e54479904dae8ea1c776de56a
gspayonk <grissobel.castro@gmail.com>
2019-10-04 16:56:35 -0400

    testing

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  render(<Dashboard />, div);
+});
+
+it('renders without crashing', () => {
+  render(<Display />, div);
 });
