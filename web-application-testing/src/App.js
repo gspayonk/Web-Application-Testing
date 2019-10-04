8c034c261c49446dd1cae4d1b4a500d066695282
gspayonk <grissobel.castro@gmail.com>
2019-10-04 16:50:07 -0400

    styling

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,103 +1,13 @@
 import React from 'react';
-import './App.css';
-import Display from './components/Display';
 import Dashboard from './components/Dashboard';
+import './App.css';
 
 
-class App extends React.Component {
 
-state = {
-    strikes: 0,
-    balls: 0,
-    fouls: 0,
-    hits: 0,
-}
+const App = () => (
+    <>
+        <Dashboard />
+    </>
+);
 
-addStrike = (event) => {
-    event.preventDefault();
-    if (this.state.strike < 2) {
-        this.setState({
-        strike: this.state.strike + 1
-        })
-    } else {
-
-        this.setState({
-        strike: 0,
-        ball: 0
-        })
-    }
-}
-    
-    addBall = (event) => {
-    event.preventDefault();
-    if (this.state.ball < 3) {
-        this.setState({
-        ball: this.state.ball + 1
-        })
-    } else {
-        this.setState({
-        strike: 0,
-        ball: 0
-        })
-    }
-    }
-
-    addFoul = (event) => {
-    event.preventDefault();
-    if (this.state.strike < 2) {
-        this.setState({
-        strike: this.state.strike + 1
-        })
-    } else {
-        this.setState({
-        ...this.state
-        })
-    }
-    }
-
-    addHit = (event) => {
-    event.preventDefault();
-    this.setState({
-        hit: this.state.hit + 1,
-        strike: 0,
-        ball: 0
-    })
-    }
-
-    render() {
-    return ( 
-        <div>
-        <Display strikes = {
-        this.state.strike
-        }
-
-        balls = {
-        this.state.ball
-        }
-        /> 
-        
-        <Dashboard 
-        
-        addStrike = {
-        this.addStrike
-        }
-
-        addBall = {
-        this.addBall
-        }
-
-        addFoul = {
-        this.addFoul
-        }
-
-        addHit = {
-        this.addHit
-        }
-
-        /> 
-
-        </div>
-    );
-    }
-}
 export default App;
--- web-application-testing/src/components/Dashboard.js
+++ web-application-testing/src/components/Dashboard.js
@@ -1,18 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useOnClick } from './useOnClick';
+import { Display } from './Display';
+import { useState } from 'react';
 
-class Dashboard extends Component {
+const Dashboard = () => {
 
-    render() {
-        
-        return (
-            <>
-            <button onClick={this.props.addStrike}>Strike</button>
-            <button onClick={this.props.addBall}>Ball</button>
-            <button onClick={this.props.addFoul}>Foul</button>
-            <button onClick={this.props.addHit}>Hit</button>
-            </>
-        )
+    const [strikes, setStrikes, onClickStrikes] = useOnClick(0);
+    const [balls, setBalls, onClickBalls] = useOnClick(0);
+    const [fouls, setFouls] = useState(0);
+
+    const onClickFoul = () => {
+    if (strikes === 0 || strikes === 1) {
+        setStrikes(strikes + 1);
+        setFouls(fouls + 1);
+    } else {
+        setFouls(fouls + 1);
     }
-}
+    };
 
-export default Dashboard; 
+    return (
+        
+    <>
+        <Display strikes={strikes} balls={balls} fouls={fouls} />
+
+        <section className = "buttons" >
+        <div className = "homeButtons" >
+
+        <button className='homeButtons__touchdown' onClick={onClickStrikes} data-testid="strikes">
+        {strikes >= 3 ? setStrikes(0) : null}
+        Strike
+        </button>
+
+        <button className="homeButtons__fieldGoal" onClick={onClickBalls} data-testid="balls">
+        {balls >= 4 ? setBalls(0) : null}
+        Ball
+        </button>
+        
+        <button className="awayButtons__touchdown" onClick={onClickFoul} data-testid="fouls">
+        Foul
+        </button>
+
+        <button className="awayButtons__fieldGoal" data-testid="hit" onClick={() => {
+            setBalls(0);
+            setStrikes(0);
+        }}
+        >
+        Hit
+        </button>
+        </div>
+    </section>
+    </>
+
+    );
+};
+
+export default Dashboard;
